9706c49edcd1038f4942169900036c33c0236f2f
Vasya Drobushkov <vasya.drobushkov@gmail.com>
2020-06-06 10:43:07 +0300

    added yellow theme

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,7 @@ module.exports = {
     "blue.css": join("source", "css", "color", "blue.css"),
     "green.css": join("source", "css", "color", "green.css"),
     "pink.css": join("source", "css", "color", "pink.css"),
+    "yellow.css": join("source", "css", "color", "yellow.css"),
   },
   output: {
     filename: "[name]",
